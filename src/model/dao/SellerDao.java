58f1bc8fa4a1c0e8e636a7ccc38e62c3aab3aa5c
Marcondes-Goncalves <marcondesgoncalves159@gmail.com>
2023-08-01 17:20:04 -0300

    Implementacao do metodo findByDepartment

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -2,6 +2,7 @@ package model.dao;
 
 import java.util.List;
 
+import model.entities.Department;
 import model.entities.Seller;
 
 public interface SellerDao {
@@ -14,6 +15,8 @@ public interface SellerDao {
     Seller findById(Integer id);
 
     //Retorna todos os vendedores cadastrados em uma lista
-    List<Seller>  findAll();
+    List<Seller> findAll();
+
+    List<Seller> findByDepartment(Department department);
 
 }
