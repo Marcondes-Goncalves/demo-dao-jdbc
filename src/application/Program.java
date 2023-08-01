41b6c55f5f0a5c6a4e1a9f246227902c5f3054b1
Marcondes-Goncalves <marcondesgoncalves159@gmail.com>
2023-08-01 14:32:28 -0300

    Classe Departamento

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,14 @@
 package application;
 
+import model.entities.Department;
+
 public class Program {
     
     public static void main(String[] args) {
         
+        Department obj = new Department(1, "Books");
+
+        System.out.println(obj);
 
 
 
@@ -13,8 +18,5 @@ public class Program {
 
 
 
-
-
-        
     }
 }
